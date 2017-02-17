54713c92bb5fc1bce631615f52551e1302f85fbe
Adriano <Adriano@DESKTOP-UV6FNPB>
2017-02-17 20:08:18 -0200

    criado o banco com a tabela CONTATOS, possuindo os atributos nome, sobrenome, telefone e email.

--- src/lppo/exercicio01/CriaTabela.java
+++ src/lppo/exercicio01/CriaTabela.java
@@ -1,25 +1,32 @@
-
 package lppo.exercicio01;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class LppoExercicio01 {
- public static void main(String[] args) {
+public class CriaTabela {
+    
+    public static void main(String[] args) {
         try {
             Class.forName("org.apache.derby.jdbc.ClientDriver");
             String url = "jdbc:derby://localhost:1527/lppo-2017-2";
             Connection conexao = DriverManager.getConnection(url, "usuario", "senha");
             System.out.println("Conexão aberta com sucesso!");
+            
+            String sql = "CREATE TABLE contatos(nome VARCHAR(15), sobrenome VARCHAR(30), telefone VARCHAR(10), email VARCHAR(30))";
+            Statement operacao = conexao.createStatement();
+            operacao.executeUpdate(sql);
+            System.out.println("Tabela criada!");
+            
         } catch (ClassNotFoundException ex) {
             System.err.println("Driver indisponivel");
-            Logger.getLogger(LppoExercicio01.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(CriaTabela.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
             System.err.println("Problema para acessar o banco!");
-            Logger.getLogger(LppoExercicio01.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(CriaTabela.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
